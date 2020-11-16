86b02521351ddada6656745c01f52db6cb301b3b
Giulio <giuliovolletti@hotmail.com>
2020-11-16 14:32:11 +0100

    commit 1

--- js/nomi-cognomi.js
+++ js/nomi-cognomi.js
@@ -1,1 +1,21 @@
-alert("hello world")
+// alert("hello world")
+
+
+// chiedi all'utente il cognome
+var cognomeUtente = prompt("inserisci qui il tuo cognome")
+console.log(cognomeUtente);
+
+// inserirlo in un array con altri cognomi (Es: 'Pesce', ‘Castenetto’, 'Passaro', 'Grimaldi', ‘Rossi’, ‘Verdi’, ‘Bianchi’).
+
+// lista cognomi
+var listaCognomi = ["pinco", "rossi", "verdi", "bianchi"];
+console.log(listaCognomi);
+// inserimento in lista
+listaCognomi.push(cognomeUtente)
+console.log(listaCognomi);
+
+
+// Stampa la lista ordinata alfabeticamente.
+listaCognomi.sort()
+console.log(listaCognomi);
+// Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova.
